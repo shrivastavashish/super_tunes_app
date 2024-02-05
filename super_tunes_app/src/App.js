df20288bf66f44311dfc8e0a7cda5a3c0081bc17
Ashish Shrivastav <shrivastav.ashish@outlook.com>
2024-02-05 23:20:18 +0530

    making boiler plate
    boiler plate work done!

---
+++ super_tunes_app/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div id="super-tunes">
+        <h2 id="st-title">SuperTunes - Songs of the Week</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ super_tunes_app/src/components/SongCard.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SongCard = () => {
+  return <div></div>;
+};
+
+export default SongCard;
